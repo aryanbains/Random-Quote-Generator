6a2af0256bb68ddcf3483b58bd7d533890c3d964
Aryan Bains <aryanbains6@gmail.com>
2024-08-01 22:54:10 +0530

    Connected API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import QuoteCard from './Components/QuoteCard/QuoteCard';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [quote, setQuote] = useState('');
+  const [savedQuotes, setSavedQuotes] = useState([]);
+
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
+  const fetchQuote = async () => {
+    const response = await fetch('https://ron-swanson-quotes.herokuapp.com/v2/quotes');
+    const data = await response.json();
+    setQuote(data[0]);
+  };
+
+  const saveQuote = (quote) => {
+    setSavedQuotes((prevQuotes) => [...prevQuotes, quote]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Ron Swanson Quotes</h1>
+      <button onClick={fetchQuote}>Get New Quote</button>
+      {quote && <QuoteCard quote={quote} onSave={saveQuote} />}
+      <h2>Saved Quotes</h2>
+      <div className="saved-quotes">
+        {savedQuotes.map((savedQuote, index) => (
+          <QuoteCard key={index} quote={savedQuote} onSave={() => {}} />
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
